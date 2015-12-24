e398d615194613d959c4feb41124fc6643e644ff
Remo Hanock <remo.hanock@gmail.com>
2015-12-24 14:46:20 +0000

    Added about and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  resources :articles
+  
+  get 'pages/home' => 'pages#home'
+  get 'pages/about' => 'pages#about'
+  get 'articles/new' => 'articles#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
